34b42f9f4535fb63d78028f6c7dcf688e1e45afd
Jan <github@23t.de>
2014-03-06 10:09:10 -0500

    add gif scanner

---
+++ scangifs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#vim:ts=2:sw=2:expandtabs
+import fnmatch
+import os
+import glob
+import json
+
+gifdir = "/silo/share/gifs"
+outfile = os.path.join(gifdir,"gifs.json");
+
+gifs = []
+for gif in glob.iglob(os.path.join(gifdir, "*", "*.gif")):
+	gifs.append(gif[len(gifdir)+1:]) # cut off gifdir
+
+f = open(outfile, 'w')
+f.write(json.dumps(gifs))
+f.close
