c19dcc43f5bf0f3c06f48823c38baee86c90bc43
Jan <github@23t.de>
2014-03-06 11:16:01 -0500

    output correct json

--- scangifs.py
+++ scangifs.py
@@ -11,7 +11,6 @@ outfile = os.path.join(gifdir,"gifs.json");
 gifs = []
 for gif in glob.iglob(os.path.join(gifdir, "*", "*.gif")):
 	gifs.append(gif[len(gifdir)+1:]) # cut off gifdir
-
 f = open(outfile, 'w')
-f.write(json.dumps(gifs))
+f.write(json.dumps({'gifs': gifs}))
 f.close
